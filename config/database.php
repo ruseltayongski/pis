307f0eff3b9b86f5418018fb32d87ac58f24b332
Carl0Bolante <98carlo.bolante@gmail.com>
2025-05-07 16:18:19 +0800

    Database config

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 
-// $host = '192.168.110.31';
-// $username = 'rtayong_31';
-// $password = 'rtayong_31';
+$host = '192.168.110.31';
+$username = 'rtayong_31';
+$password = 'rtayong_31';
 
-$host = 'localhost';
-$username = 'root';
-$password = '';
+// $host = 'localhost';
+// $username = 'root';
+// $password = '';
 
 return [
 
